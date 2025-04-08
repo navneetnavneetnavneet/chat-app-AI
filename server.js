0ff1c1886cbab741035154c651fc82b7cfe42aec
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-04-08 15:25:33 +0530

    socket.io setup and add authentication middleware

--- server.js
+++ server.js
@@ -3,10 +3,48 @@ require("dotenv").config({
 });
 const http = require("http");
 const app = require("./app");
+const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("./utils/ErrorHandler");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.use(async (socket, next) => {
+  try {
+    const token =
+      socket.handshake.auth?.token ||
+      socket.handshake.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return next(new ErrorHandler("Authentication error", 401));
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return next(new ErrorHandler("Authentication error", 401));
+    }
+    
+    socket._id = decoded;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("event", (data) => {
+    /* … */
+  });
+  socket.on("disconnect", () => {
+    /* … */
+  });
+});
 
 // start server
 server.listen(port, () => {
